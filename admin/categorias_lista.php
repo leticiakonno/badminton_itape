df1dca09fc8d6c16b81274b8ac80534a60e0d330
leheduarda92-boop <leheduarda92@gmail.com>
2025-11-24 10:32:43 -0300

    new file:   admin/categorias_lista.php

---
+++ admin/categorias_lista.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categorias</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+</head>
+<body>
+    <main class="container">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <h1 class="breadcrumb alert-info">Lista de Categorias</h1>
+
+
+    </main>
+</body>
+</html>
