f00983030722ea9255be1a6432b7f1b10754ad61
leticiakonno <leticia.konno13@gmail.com>
2025-11-25 18:57:18 -0300

    modified:   bkp_db/dbbadminton.sql

--- bkp_db/dbbadminton.sql
+++ bkp_db/dbbadminton.sql
@@ -107,9 +107,8 @@ INSERT INTO tbparceiros(id_parceiro,nome_parceiro,descri_parceiro,img_parceiro) 
     (3,'Universal Chemical', 'Uma das maiores empresas da região, são apoiadores do esporte, sempre prestando suporte à equipe.', 'chemical.jpeg'),
     (4,'Ligia', 'Uma das maiores apoiadoras da equipe, faz por amor ao esporte e toda a equipe. Coração e alma enormes.', 'ligia.jpeg'),
     (5, 'Cristina Mori e Dr.Thiago', 'Médicos e pais de atleta, proporcionam acompanhamento médico e apoiam a saúde física e mental dos nossos atletas.', 'cristinaethiago.jpeg'),
-    (6, 'Marcia Maeseki', 'Mãe de atletas e grande incentivadora do esporte, sempre presente nos torneios e eventos.', 'marcia.jpeg'),
-    (7, 'Roberto Ueno e Laura Takamori', 'Pais de atletas e grandes apoiadores do esporte, prestativos e sempre à disposição.', 'robertoelaura.jpeg'),
-    (8, 'Cristiane Matiazzo', 'Fisioterapeuta voluntária que cuida dos atletas, prevenindo lesões e promovendo a recuperação física.', 'cristiane.jpeg');
+    (6, 'Roberto Ueno e Laura Takamori', 'Pais de atletas e grandes apoiadores do esporte, prestativos e sempre à disposição.', 'robertoelaura.jpeg'),
+    (7, 'Cristiane Matiazzo', 'Fisioterapeuta voluntária que cuida dos atletas, prevenindo lesões e promovendo a recuperação física.', 'cristiane.jpeg');
 
 -- Estrutura da tabela tbtorneios
 CREATE TABLE tbtorneios(
@@ -181,7 +180,7 @@ ALTER TABLE tbtecnicos
     MODIFY id_tecnico INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 ALTER TABLE tbparceiros
-    MODIFY id_parceiro INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+    MODIFY id_parceiro INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 
 -- Limitadores e referências da Chave Estrangeira
