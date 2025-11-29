59b8d8e87ffbfe8916984eff58876090d7119b99
leheduarda92-boop <leheduarda92@gmail.com>
2025-11-29 11:31:59 -0300

    new file:   modelo_bootstrap - Copia.php

---
+++ modelo_bootstrap - Copia.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modelo</title>
+    <!-- Link CSS do Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Link para CSS Específico -->
+    <link rel="stylesheet" href="css/meu_estilo.css">
+</head>
+<body>
+
+<!-- Link arquivos Bootstrap js -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>    
+</body>
+</html>
