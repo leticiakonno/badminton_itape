b46cf2fbbbb46009bf942154958e4a8afa6cbb5f
leticiakonno <leticia.konno13@gmail.com>
2025-12-02 21:06:21 -0300

    new file:   atletas_categorias.php 	modified:   atletas_destaque.php 	new file:   atletas_por_categoria.php

--- atletas_destaque.php
+++ atletas_destaque.php
@@ -53,7 +53,7 @@ $totalRows_destaque      =   ($lista_destaque)->num_rows;
                     <strong><?php echo $row_destaque['nome_atleta']; ?></strong>
                 </h3>
                 <p class="text-left">
-                <?php echo mb_strimwidth ($row_destaque['descri_atleta'],0,42,"...");?>
+                <?php echo mb_strimwidth ($row_destaque['descri_atleta'],0,45,"...");?>
                 </p>          
                     <a 
                         href="atletas_detalhe.php?id_atleta=<?php echo $row_destaque['id_atleta']; ?>"
