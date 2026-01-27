571d719b179c46cceca0b6ff7c16faf866e3e6bf
leheduarda92-boop <leheduarda92@gmail.com>
2026-01-27 09:12:46 -0300

    modified:   css/meu_estilo.css 	modified:   historia.php 	new file:   index.php

--- historia.php
+++ historia.php
@@ -11,13 +11,13 @@
 </head>
 <body class="fundofixo">
 <?php include('menu_publico.php'); ?>
-    <main class="container">
+    <main class="containerhistoria">
     <div class="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-0 col-md-12" > <!-- abre dimensionamento -->
         <div class="form-container tabela-branca">
-            <br>
+        <br>
         <h2 class="fundoatletas titulo text-center" style="border: 30px;"><strong>Saiba mais sobre nossa história:</strong></h2>
         <br>
-        <img src="imagens/familia.jpeg.png" style="float: right; margin-left: 20px; width: 600px; height: 400px;border-radius: 15px; ">
+        <img src="imagens/familia.jpeg.png" style="float: right; margin: 20px; width: 500px; height: 600px;border-radius: 8px; ">
         <p class="historia">A história do badminton em Itapetininga se funde com  a dedicação da pioneira <strong>Leiko Konno.</strong>
         </strong>O esporte, inicialmente desconhecido na cidade, começou a ganhar forma após o marido dela, Shogo,
         trazer raquetes do Japão.O que era uma prática informal em praças tornou-se um projeto sério quando Leiko, 
@@ -35,5 +35,7 @@
 
         </div> <!--fecha tabela-->
     </div> <!--fecha dimensionamento-->
+<?php include('rodape.php'); ?>
+
 </main>
 </body>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badminton Itapetininga</title>
+</head>
+<body class="fundofixo">
+    <!-- MENU -->
+    <a name="home"></a>
+    <?php include('menu_publico.php'); ?>
+
+    <main class="container">
+        <!-- CARROUSSEL -->
+        <?php include('carroussel.php'); ?>
+
+        <!-- Torneios -->
+        <a name="torneios"></a>
+        <hr>
+        <?php include('torneios_destaque.php'); ?>
+
+        <!-- Atletas -->
+        <a name="atletas"></a>
+        <hr>
+        <?php include('atletas_geral.php'); ?>
+
+         <!-- Historia -->
+        <a name="historia"></a>
+        <hr>
+        <?php include('historia.php'); ?>
+
+        <!-- RODAPÉ -->
+        <footer>
+            <?php include('rodape.php'); ?>
+            <a name="contato"></a>
+        </footer>
+    </main>
+</body>
+</html>
