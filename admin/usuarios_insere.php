34bf81ebf648de7aa7081b6b97c785888e5b5490
Letícia Konno <leticia.konno13@gmail.com>
2026-02-09 21:04:22 -0300

    modified:   admin/acesso_com.php 	modified:   admin/usuarios_insere.php

--- admin/usuarios_insere.php
+++ admin/usuarios_insere.php
@@ -48,6 +48,7 @@ if($_POST){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usuários Insere</title>
+    <script src="https://kit.fontawesome.com/d03c290dd3.js" crossorigin="anonymous"></script>
     <!-- Link CSS do Bootstrap -->
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <!-- Link para CSS Específico -->
