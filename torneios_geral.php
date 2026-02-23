ad7e7e30e30e8df89bf28087fc045e2f54a7425f
Letícia Konno <leticia.konno13@gmail.com>
2026-02-23 20:32:14 -0300

    modified:   historiaresumo.php 	modified:   noticias_geral.php 	modified:   torneios_geral.php

--- torneios_geral.php
+++ torneios_geral.php
@@ -17,7 +17,7 @@ $totalRows  =   ($lista)->num_rows;
 
 <body class="fundofixo fontetabela">
     <main class="container">
-    <h2 class="fundocategoria categoriageral text-center titulo"><strong>Nossos caminhos nas quadras:</strong></h2>
+    <h2 class="fundocategoria categoriageral text-center titulo"><strong>TRAJETÓRIA DE CAMPEÕES   <i class="fa-solid fa-medal"></i></strong></h2>
     <br><br>
     <div class="container">
         <div class="row">
