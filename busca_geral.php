fd0365ac31dba17e63e3a6c35a2a776aba49a91f
Leticia Carvalho <leheduarda92@gmail.com>
2026-02-24 19:27:35 -0300

    modified:   busca_geral.php

--- busca_geral.php
+++ busca_geral.php
@@ -36,22 +36,22 @@ if ($filtro != "") {
 <head>
     <meta charset="UTF-8">
     <title>Resultado da Busca</title>
+     <!-- Link CSS do Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Link para CSS Específico -->
+    <link rel="stylesheet" href="css/meu_estilo.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-<body class="container">
-    <?php include('menu_publico.php'); ?>
-    <h2>Você pesquisou por: "<?php echo $filtro; ?>"</h2>
-    <hr>
-
+ <?php include('menu_publico.php'); ?>
+<body class="fundofixo fontetabela"> 
 <main class="container">
-    <h2 class="breadcrumb alert-danger fundoatletas titulo">
+    <h2 class="breadcrumb  fundoatletas titulo">
           <a href="javascript:window.history.go(-1)" class="btn btntotal">
         <span class="glyphicon glyphicon-chevron-left"></span>
     </a>
     Resultados para: "<?php echo $filtro; ?>"</h3>
     <hr>
     </h2>
-
     <div class="row">
         <?php 
         if(isset($lista) && $lista->num_rows > 0) {
