070a88fd3e004cd16be8707eb2ca22a2a7f6c9dc
h6mura <mavelyn2808@gmail.com>
2026-02-25 20:40:08 -0300

    modified:   admin/usuarios_atualiza.php 	modified:   admin/usuarios_exclui.php 	modified:   admin/usuarios_insere.php

--- admin/usuarios_exclui.php
+++ admin/usuarios_exclui.php
@@ -1,31 +1,42 @@
 <?php
-//Incluir o Sistema de Autenticação
+// Incluir o Sistema de Autenticação
 include("acesso_sup.php");
 
 // Incluir o arquivo e fazer a conexão
 include("../Connections/conn_atletas.php");
 
 // Definindo o USE do banco de dados
-mysqli_select_db($conn_atletas,$database_conn);
+mysqli_select_db($conn_atletas, $database_conn);
 
 // Definindo e recebendo dados para consulta
-$tabela_delete  =   "tbusuarios";
-$id_tabela_del  =   "id_usuario";
-$id_filtro_del  =   $_GET['id_usuario'];
+$tabela_delete = "tbusuarios";
+$id_tabela_del = "id_usuario";
+$id_filtro_del = $_GET['id_usuario'];
+
+// Primeiro, buscar a foto para excluir do diretório
+$consulta_foto = "SELECT foto_usuario FROM $tabela_delete WHERE $id_tabela_del = $id_filtro_del";
+$resultado_foto = $conn_atletas->query($consulta_foto);
+$dados_foto = $resultado_foto->fetch_assoc();
+
+// Excluir foto do diretório se existir
+if (!empty($dados_foto['foto_usuario'])) {
+    $caminho_foto = "../imagens/usuarios/" . $dados_foto['foto_usuario'];
+    if (file_exists($caminho_foto)) {
+        unlink($caminho_foto);
+    }
+}
 
 // SQL para exclusão
-$deleteSQL  =   "
-                DELETE
-                FROM    ".$tabela_delete."
-                WHERE   ".$id_tabela_del."=".$id_filtro_del.";
-                ";
-$resultado  =   $conn_atletas->query($deleteSQL);
+$deleteSQL = "
+    DELETE
+    FROM $tabela_delete
+    WHERE $id_tabela_del = $id_filtro_del
+";
 
-// Após a ação a página será redirecionada
-$destino    =   "usuarios_lista.php";
-if(mysqli_insert_id($conn_atletas)){
-    header("Location: $destino");
-}else{
-    header("Location: $destino");
-};
+$resultado = $conn_atletas->query($deleteSQL);
+
+// Redirecionamento
+$destino = "usuarios_lista.php";
+header("Location: $destino");
+exit;
 ?>
--- admin/usuarios_insere.php
+++ admin/usuarios_insere.php
@@ -5,34 +5,37 @@ include("acesso_sup.php");
 // Incluir o arquivo e fazer a conexão
 include("../Connections/conn_atletas.php");
 
-if($_POST){
+if ($_POST) {
 
     // RECEBE DADOS
-    $login_usuario  = $_POST['login_usuario'];
-    $senha_usuario  = $_POST['senha_usuario'];
-    $nivel_usuario  = $_POST['nivel_usuario'];
+    $login_usuario = $_POST['login_usuario'];
+    $senha_usuario = $_POST['senha_usuario'];
+    $nivel_usuario = $_POST['nivel_usuario'];
 
     // ==============================================
     // UPLOAD DA IMAGEM
     // ==============================================
-    $foto_usuario = ""; // variavel que VAI para o banco!
+    $foto_usuario = ""; // valor que vai para o banco
 
-    if(isset($_FILES['foto_usuario']) && $_FILES['foto_usuario']['error'] == 0){
+    if (isset($_FILES['foto_usuario']) && $_FILES['foto_usuario']['error'] == 0) {
+        $nomeArquivo = time() . "_" . $_FILES['foto_usuario']['name'];
+        $tempArquivo = $_FILES['foto_usuario']['tmp_name'];
 
-        $foto_usuario = $_FILES['foto_usuario']['name'];
-        $tmp_img      = $_FILES['foto_usuario']['tmp_name'];
+        // Criar pasta se não existir
+        if (!is_dir('../imagens/usuarios/')) {
+            mkdir('../imagens/usuarios/', 0777, true);
+        }
 
-        // Caminho final
-        $destino      = "../imagens/" . $foto_usuario;
+        $destino = "../imagens/usuarios/" . $nomeArquivo;
 
-        // Move arquivo
-        move_uploaded_file($tmp_img, $destino);
+        if (move_uploaded_file($tempArquivo, $destino)) {
+            $foto_usuario = $nomeArquivo;
+        }
     }
 
     // ==============================================
     // INSERT
     // ==============================================
-
     $sql = "
         INSERT INTO tbusuarios
             (login_usuario, senha_usuario, nivel_usuario, foto_usuario)
@@ -57,12 +60,22 @@ if($_POST){
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <!-- Link para CSS Específico -->
     <link rel="stylesheet" href="../css/meu_estilo.css">
+    <style>
+        #preview {
+            max-height: 150px;
+            margin-bottom: 10px;
+            display: none;
+            border: 1px solid #ddd;
+            padding: 5px;
+            border-radius: 4px;
+        }
+    </style>
 </head>
 <body class="fundofixo fontetabela">
 <?php include("menu_adm.php"); ?>
-    <main class="container">
- <div class="row">
-        <div class="col-xs-12 col-sm-offset-3 col-sm-6" > <!-- abre dimensionamento -->
+<main class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-3 col-sm-6">
             <h2 class="breadcrumb fundousuarios text-center titulo">
                 <a href="usuarios_lista.php">
                     <button class="btn btntotal bg-danger text-white">
@@ -71,123 +84,115 @@ if($_POST){
                 </a>
                 Inserir Usuários
             </h2>
-            <div class="thumbnail"> <!-- abre thumbnail -->
+            <div class="thumbnail">
                 <div class="alert alert">
-                    <form 
-                        action="usuarios_insere.php"
-                        enctype="multipart/form-data"
-                        method="post"
-                        id="form_insere_usuario"
-                        name="form_insere_usuario"
-                    >
-                    
-                       <!-- file img_atleta -->
-                    <label for="foto_usuario">Imagem:</label>
+                    <form action="usuarios_insere.php"
+                          enctype="multipart/form-data"
+                          method="post"
+                          id="form_insere_usuario"
+                          name="form_insere_usuario">
+
+                        <!-- Imagem com pré-visualização -->
+                        <label for="foto_usuario">Imagem:</label>
                         <div class="input-group">
                             <span class="input-group-addon">
                                 <span class="glyphicon glyphicon-picture"></span>
                             </span>
-                            <!-- Exibir a imagem a ser inserida -->
-                            <img 
-                                src="" 
-                                alt=""
-                                name="imagem"
-                                id="imagem"
-                                class="img-responsive"
-                                style="max-height: 150px;"
-                            >
-                            <input 
-                                type="file" 
-                                name="foto_usuario" 
-                                id="foto_usuario"
-                                class="form-control"
-                                accept="image/*"
-                            >
-                        </div> <!-- fecha input-group -->
-                        <!-- fecha file imagem_produto -->
+                            <img src="#"
+                                 alt="Prévia da imagem"
+                                 id="preview"
+                                 class="img-responsive">
+                            <input type="file"
+                                   name="foto_usuario"
+                                   id="foto_usuario"
+                                   class="form-control"
+                                   accept="image/*"
+                                   onchange="previewImage(event)">
+                        </div>
                         <br>
 
-                         <!-- text login_usuario -->
-                         <label for="login_usuario">Login:</label>
+                        <!-- Login -->
+                        <label for="login_usuario">Login:</label>
                         <div class="input-group">
                             <span class="input-group-addon">
                                 <span class="glyphicon glyphicon-user"></span>
                             </span>
-                            <input 
-                                type="text" 
-                                name="login_usuario" 
-                                id="login_usuario"
-                                class="form-control"
-                                placeholder="Digite o seu login."
-                                maxlength="30"
-                                required
-                            >
+                            <input type="text"
+                                   name="login_usuario"
+                                   id="login_usuario"
+                                   class="form-control"
+                                   placeholder="Digite o seu login."
+                                   maxlength="30"
+                                   required>
                         </div>
                         <br>
 
-                        <!-- text senha_usuario -->
-                        <label for="senha_usuario">Senha: </label>
+                        <!-- Senha -->
+                        <label for="senha_usuario">Senha:</label>
                         <div class="input-group">
                             <span class="input-group-addon">
                                 <span class="glyphicon glyphicon-qrcode"></span>
                             </span>
-                            <input 
-                                type="password" 
-                                name="senha_usuario" 
-                                id="descri_produto"
-                                class="form-control"
-                                placeholder="Digite a senha desejada."
-                                maxlength="8"
-                                required
-                            >
+                            <input type="password"
+                                   name="senha_usuario"
+                                   id="senha_usuario"
+                                   class="form-control"
+                                   placeholder="Digite a senha desejada."
+                                   maxlength="8"
+                                   required>
                         </div>
                         <br>
 
-                        <!-- radio nivel_usuario -->
-                        <label for="nivel_usuario_c">Nível do usuário?</label>
+                        <!-- Nível -->
+                        <label for="nivel_usuario">Nível do usuário?</label>
                         <div class="input-group">
                             <label for="nivel_usuario_c" class="radio-inline">
-                                <input 
-                                    type="radio"
-                                    name="nivel_usuario"
-                                    id="nivel_usuario"
-                                    value="com"
-                                    checked
-                                >
+                                <input type="radio"
+                                       name="nivel_usuario"
+                                       id="nivel_usuario_c"
+                                       value="com"
+                                       checked>
                                 Comum
                             </label>
                             <label for="nivel_usuario_s" class="radio-inline">
-                                <input 
-                                    type="radio"
-                                    name="nivel_usuario"
-                                    id="nivel_usuario"
-                                    value="sup"
-                                >
+                                <input type="radio"
+                                       name="nivel_usuario"
+                                       id="nivel_usuario_s"
+                                       value="sup">
                                 Supervisor
                             </label>
                         </div>
                         <br>
 
-                      
-                         <!-- btn enviar -->
-                         <input 
-                            type="submit" 
-                            value="Cadastrar"
-                            name="enviar"
-                            id="enviar"
-                            role="button"
-                            class="btn btntotal btn-block"
-                        >
+                        <!-- Botão enviar -->
+                        <input type="submit"
+                               value="Cadastrar"
+                               name="enviar"
+                               id="enviar"
+                               role="button"
+                               class="btn btntotal btn-block">
                     </form>
-
                 </div>
             </div>
         </div>
     </div>
 </main>
+
+<script>
+function previewImage(event) {
+    var reader = new FileReader();
+    reader.onload = function() {
+        var output = document.getElementById('preview');
+        output.src = reader.result;
+        output.style.display = 'block';
+    };
+    reader.readAsDataURL(event.target.files[0]);
+}
+</script>
+
 <!-- Link arquivos Bootstrap js -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>    
+<script src="../js/bootstrap.min.js"></script>
 <footer>
     <?php include('../rodape.php'); ?>
 </footer>
