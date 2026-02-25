ec56606c1d35c7b62b5225fcd19ca5467376e1a5
Leticia Carvalho <leheduarda92@gmail.com>
2026-02-24 21:44:07 -0300

    modified:   rodape_contato.php

--- rodape_contato.php
+++ rodape_contato.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Após 15 segundos a página sera redirecionada para index.php -->
     <meta http-equiv="refresh" content="15;URL=index.php">
+        <!-- Link CSS do Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Link para CSS Específico -->
+    <link rel="stylesheet" href="css/meu_estilo.css">
     <title>Verificação do Contato</title>
 </head>
 <body class="fundo2">
@@ -36,6 +40,9 @@
         </div> <!-- fecha jumbotron -->
 
     </section>
+    <!-- Link arquivos Bootstrap js -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script> 
 </main>
 
 <!-- rodapé -->
