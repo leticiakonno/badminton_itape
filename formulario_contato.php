e2cf1a04762eb91529ffbd03cfc8fdc1639f5ba9
Letícia Konno <leticia.konno13@gmail.com>
2026-02-25 20:09:47 -0300

    modified:   formulario_contato.php

--- formulario_contato.php
+++ formulario_contato.php
@@ -17,16 +17,34 @@
     <section>
         <div class="jumbotron fundoatletas">
             <h1><strong><i>Agradecemos seu contato!</i> </strong></h1>
-            <?php
-                $destino        =   "leticia.konno@hotmail.com";
-                $nome_formulario   =   $_POST['nome_formulario'];
-                $date_formulario = $_POST['date_formulario'];
+                <?php
+                $destino = "leticia.konno@hotmail.com";
+
+                // Coleta dos dados do formulário
+                $nome_formulario     = $_POST['nome_formulario'];
+                $date_formulario     = $_POST['date_formulario'];
                 $telefone_formulario = $_POST['telefone_formulario'];
-                $email_formulario  =   $_POST['email_formulario'];
-                $assunto_contato = $_POST['assunto_contato'];
-                $msg_contato    =   "Mensagem de:".$nome_formulario."\n".$_POST['comentarios_contato'];
-                $mailsend   =   mail($destino,"Formulário de comentário",$msg_contato,"From:",$email_formulario);
-            ?>
+                $email_formulario    = $_POST['email_formulario'];
+                $assunto_contato     = $_POST['assunto_contato'];
+                $comentarios_contato = $_POST['comentarios_contato'];
+
+                // Montagem do corpo do e-mail organizado
+                $msg_contato  = "Novo contato via formulário:\n\n";
+                $msg_contato .= "Nome: " . $nome_formulario . "\n";
+                $msg_contato .= "E-mail: " . $email_formulario . "\n";
+                $msg_contato .= "Telefone: " . $telefone_formulario . "\n";
+                $msg_contato .= "Data: " . $date_formulario . "\n";
+                $msg_contato .= "Assunto: " . $assunto_contato . "\n\n";
+                $msg_contato .= "Mensagem:\n" . $comentarios_contato;
+
+                // Cabeçalhos para evitar Spam e permitir resposta direta
+                $headers  = "From: Leticia Dev <contato@seudominio.com>\r\n"; // Use um e-mail do seu domínio
+                $headers .= "Reply-To: " . $email_formulario . "\r\n";
+                $headers .= "X-Mailer: PHP/" . phpversion();
+
+                // Envio do e-mail
+                $mailsend = mail($destino, "Formulário: " . $assunto_contato, $msg_contato, $headers);
+                ?>
             <div class="text-center">
                 <p>
                     Obrigado por enviar seus comentários,
